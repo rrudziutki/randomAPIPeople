6a941b9ebce91e8f7526a469e5fa0656682bbc94
rrudziutki <r.makuchowski1@gmail.com>
2022-03-27 18:32:32 +0200

    Initial commit

---
+++ randomApiPeople/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomApiPeople
+//
+//  Created by Remik Makuchowski on 27/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
